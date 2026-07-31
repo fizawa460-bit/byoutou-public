2e5b83bc8433054bc42448e115b6d15312e0ef77
fizawa460-bit <fizawa460@gmail.com>
2026-07-31 23:42:43 +0900

    端末別の操作案内を表示

--- poc/godot-map-3d/scripts/main.gd
+++ poc/godot-map-3d/scripts/main.gd
@@ -5,8 +5,9 @@ extends Node3D
 
 func _ready() -> void:
     world.build_finished.connect(_on_build_finished)
+    if DisplayServer.is_touchscreen_available() or OS.has_feature("mobile"):
+        $HUD/Info.text = "左スティック: 移動  右側スワイプ: 視点\nJSON → 3D PoC"
 
 func _on_build_finished(map_name: String, warnings: PackedStringArray) -> void:
     $HUD/Info.text += "\nMap: %s" % map_name
     warnings_label.text = "\n".join(warnings)
-
